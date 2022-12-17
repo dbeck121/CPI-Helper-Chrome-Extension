29e90d8fb850c84fbb7364ca514e1fdb5a51899f
Tobias Hennekes <62445549+thennekes@users.noreply.github.com>
2022-12-17 10:17:01 +0100

    release 3.0.0
    release 3.0.0

--- scripts/whatsNew.js
+++ scripts/whatsNew.js
@@ -33,7 +33,7 @@ async function whatsNewCheck(showOnlyOnce = true) {
           </div>
     <div class="twelve wide column">
          <div class="ui header">This release is sponsored by Figaf </div>
-          <p>Figaf makes it easier for you to manage your SAP Cloud Integration, so you can focus on development not paperwork.</p>
+          <p>Figaf makes it easier for you to manage your SAP Cloud Integration, so you can focus on development, not paperwork.</p>
           <p>Figaf is an all-in-one solution that covers testing, transport, alerting and migration.
             Read more and try it for free <a href="https://figaf.com/cpihelper-and-figaf" target="_blank">here</a>.
           </p>
@@ -122,9 +122,9 @@ Follow us on <a href="https://www.linkedin.com/company/kangoolutions" target="_b
    </a>
    </div>
 
-          <p>To learn more about CPI Helper features and whats new on our <a href="https://github.com/dbeck121/CPI-Helper-Chrome-Extension" target="_blank">Github
+          <p>To learn more about CPI Helper features and what's new on our <a href="https://github.com/dbeck121/CPI-Helper-Chrome-Extension" target="_blank">Github
           Page</a>.</p>
-          <p>Unfortunately SAP does not work with us together and does not inform us when the APIs changes. So be gentle if sth. does not work. we do this in our free time and sometimes it takes a while to adapt to SAP changes.</p>
+          <p>Unfortunately, SAP does not work with us together and does not inform us when the APIs change. So be gentle if sth. does not work. we do this in our free time and sometimes it takes a while to adapt to SAP changes.</p>
         </div>
        <div class="ui bottom attached tab segment active" data-tab="three">
  
@@ -151,7 +151,7 @@ Follow us on <a href="https://www.linkedin.com/company/kangoolutions" target="_b
     Take Part
   </div>
 </h3>
-              <p>The CPI Helper is free and Open Source. If you want to contribute (especially improve overall CPI Helper visual design. We really aren't frontend developers) or you have found any bugs than have a look at our <a href="https://github.com/dbeck121/CPI-Helper-Chrome-Extension" target="_blank">GitHub Page</a> and our <a href="https://kangoolutions.com" target="_blank">Homepage</a>. You can find the main developer Dominic Beckbauer on <a href="https://www.linkedin.com/in/dominic-beckbauer-515894188/">LinkedIn</a></p>
+              <p>The CPI Helper is free and Open Source. If you want to contribute (especially improve overall CPI Helper visual design. We really aren't frontend developers) or you have found any bugs then have a look at our <a href="https://github.com/dbeck121/CPI-Helper-Chrome-Extension" target="_blank">GitHub Page</a> and our <a href="https://kangoolutions.com" target="_blank">Homepage</a>. You can find the main developer Dominic Beckbauer on <a href="https://www.linkedin.com/in/dominic-beckbauer-515894188/">LinkedIn</a></p>
      
               <h3 class="ui header">
               <i class="glasses icon"></i>
