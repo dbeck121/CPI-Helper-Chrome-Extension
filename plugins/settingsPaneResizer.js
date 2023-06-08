51e7075ef4aef8de970f0e66682d132c4d1cd72c
Philippe.Addor <philippe.addor@bmtg.ch>
2023-06-08 10:45:17 +0200

    use jquery again for opening of pane

--- plugins/settingsPaneResizer.js
+++ plugins/settingsPaneResizer.js
@@ -96,15 +96,13 @@ var plugin = {
                 // create inline script to trigger the pane restore button in UI5
                 const scriptElement = document.createElement('script');
                 scriptElement.setAttribute("id", "settingsPaneResizerScript");
-                scriptElement.innerHTML = `		
-                            console.log("go");
-                            window.sap.ui.getCore().byId( $('[id $="--iflowSplitter-bar0-restore-btn"]').eq(0).attr("id")).firePress(); 					
-                            var s = window.sap.ui.getCore().byId( $('[id^="__xmlview"][id$="-iflowSplitter"]').eq(0).attr("id") );
-                            s.getContentAreas()[0].setLayoutData(new sap.ui.layout.SplitterLayoutData({ size: "${(100-newHeightInPct) + "%"}" }));
-                            s.getContentAreas()[1].setLayoutData(new sap.ui.layout.SplitterLayoutData({ size: "${newHeightInPct + "%"}" }));
-                            s.invalidate();
-                    
-                `;
+                scriptElement.innerHTML = `					                    						
+                        $( document ).ready( window.sap.ui.getCore().byId( $('[id $="--iflowSplitter-bar0-restore-btn"]').eq(0).attr("id")).firePress() );
+						var s = window.sap.ui.getCore().byId( $('[id^="__xmlview"][id$="-iflowSplitter"]').eq(0).attr("id") );
+						s.getContentAreas()[0].setLayoutData(new sap.ui.layout.SplitterLayoutData({ size: "${(100-newHeightInPct) + "%"}" }));
+						s.getContentAreas()[1].setLayoutData(new sap.ui.layout.SplitterLayoutData({ size: "${newHeightInPct + "%"}" }));
+						s.invalidate();
+                    `;
                                 
                 document.head.appendChild(scriptElement);
                             
