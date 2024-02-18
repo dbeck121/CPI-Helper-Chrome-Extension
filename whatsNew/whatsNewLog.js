32b8382526e20a026452fc2a5871f11696532524
Dominic Beckbauer <github@beckbauer.de>
2024-02-18 12:35:46 +0100

    whats new update

--- whatsNew/whatsNewLog.js
+++ whatsNew/whatsNewLog.js
@@ -5,6 +5,10 @@
 //     } ,... //multiple
 // ]
 const whats_new_log = [
+    {
+        "header": "Feature",
+        "description": "New Timeline Plugin: Get overview about message flow. Thanks to Gregor Schütz from <a href='https://www.agilita.ch/'>AGILITA AG</a>"
+    },
 
     {
         "header": "Feature",
