e54801fa022b0ee61a965ea0afc217bcf50c1208
DevGregor <gregor.b.schuetz@gmail.com>
2024-05-10 11:28:42 +0200

    hover and input value adjustments

--- plugins/credentialHelper.js
+++ plugins/credentialHelper.js
@@ -16,6 +16,35 @@ var plugin = {
             //prepare array to be filled
             var securityMaterialList;
 
+            //the ids of the input fields vary from adapter to adapter
+            //if the ids are changed or new adapters were added we need to adjust/expand this check with the new ids
+            //these ids were checked for both Neo and CF environments for the following adapters... (07. May 2024)
+            //AMQP, Ariba, AS2, AS4, Elster, Facebook, FTP, OData, HTTP, IDOC, JDBC, Kafka, LDAP, Mail, MDI, ODC, SFTP, SOAP, SuccessFactors, Twitter, XI
+            const patterns = [
+                'credentialName',
+                'ldapCredentialName',
+                'alias',
+                'user',
+                'samlAlias',
+                'authPrivateKeyAlias',
+                'credential_name',
+                'encryptionKeyExpression',
+                'signatureKeyExpression',
+                'oAuthAppId',
+                'oAuthAppSecret',
+                'oAuthAccessToken',
+                'tokenCredential',
+                'consumerKey',
+                'consumerSecret',
+                'accessToken',
+                'accessTokenSecret',
+                'BasicAuthCredentialName',
+                'ClientCertificateAlias',
+                'odataCertAuthPrivateKeyAlias',
+                'mdiCredentialName',
+                'privateKeyAlias'
+            ];
+            
             //function to handle input typing and show matching suggestions as divs
             function handleTyping(event) {
                 //find matching security materials and certificates based on user input
@@ -25,9 +54,11 @@ var plugin = {
 
                 //indicate that there is no matching alias
                 var hasMatches = true;
-                if(matchingAliases.length == 0){
+                if(matchingAliases.length == 0 && event.target.value != ""){
                     matchingAliases.push("No matching alias found");
                     hasMatches = false;
+                }else if(matchingAliases.length == 0){
+                    matchingAliases = securityMaterialList;
                 }
         
                 //remove any existing dropdown to avoid duplicates
@@ -65,28 +96,20 @@ var plugin = {
                 //populate the dropdown with matching items as selectable divs
                 matchingAliases.forEach(option => {
                     const item = document.createElement('div');
-                    item.classList.add('credentialHelperDropdown-item');
                     item.textContent = option;
                     item.style.padding = '8px 12px';
                     item.style.borderBottom = '1px solid #eee';
                     
                     if(hasMatches == true){
-                        //change cursor
                         item.style.cursor = 'pointer';
+                        item.classList.add('credentialHelperDropdown-item');
 
-                        //highlight the item on hover
-                        item.addEventListener('mouseover', () => {
-                            item.style.backgroundColor = '#f1f1f1';
-                        });
-            
-                        //remove the highlight when not hovered over
-                        item.addEventListener('mouseout', () => {
-                            item.style.backgroundColor = '';
-                        });
-            
                         //set the input field value to the selected option on click
                         item.addEventListener('click', () => {
-                            event.target.value = option;
+                            //we need to reselect the input field because SAP changes the id or recreates the input field for some reason
+                            const selector = patterns.map(pattern => `input[id*="__input_"][id*="${pattern}"]`).join(', ');
+                            var input = document.querySelector(selector);
+                            input.value = option;
                             dropdown.remove(); //close the dropdown after selection
                         });
                     }
@@ -111,35 +134,9 @@ var plugin = {
             }
             
             document.addEventListener('click', async function (event) {
-                //check if a credential name or private key alias input was clicked
-                //the ids of the input fields vary from adapter to adapter
-                //if the ids are changed or new adapters were added we need to adjust/expand this check with the new ids
-                //these ids were checked for both Neo and CF environments for the following adapters... (07. May 2024)
-                //AMQP, Ariba, AS2, AS4, Elster, Facebook, FTP, OData, HTTP, IDOC, JDBC, Kafka, LDAP, Mail, MDI, ODC, SFTP, SOAP, SuccessFactors, Twitter, XI
-                if (event.target.tagName === 'INPUT' && event.target.id.includes('input') && 
-                    (event.target.id.includes('credentialName') ||
-                    event.target.id.includes('ldapCredentialName') ||
-                    event.target.id.includes('alias') ||
-                    event.target.id.includes('user') ||
-                    event.target.id.includes('samlAlias') ||
-                    event.target.id.includes('authPrivateKeyAlias') ||
-                    event.target.id.includes('credential_name') ||
-                    event.target.id.includes('encryptionKeyExpression') ||
-                    event.target.id.includes('signatureKeyExpression') ||
-                    event.target.id.includes('oAuthAppId') ||
-                    event.target.id.includes('oAuthAppSecret') ||
-                    event.target.id.includes('oAuthAccessToken') ||
-                    event.target.id.includes('tokenCredential') ||
-                    event.target.id.includes('consumerKey') ||
-                    event.target.id.includes('consumerSecret') ||
-                    event.target.id.includes('accessToken') ||
-                    event.target.id.includes('accessTokenSecret') ||
-                    event.target.id.includes('BasicAuthCredentialName') ||
-                    event.target.id.includes('ClientCertificateAlias') ||
-                    event.target.id.includes('odataCertAuthPrivateKeyAlias') ||
-                    event.target.id.includes('mdiCredentialName') ||
-                    event.target.id.includes('privateKeyAlias'))) {
-                    
+                console.log(event)
+                //check if a credential name or private key alias input was clicked and is not in readonly mode
+                if (event.target?.attributes?.readonly?.textContent != 'readonly' && event.target.tagName === 'INPUT' && event.target.id.includes('__input_') && patterns.some(pattern => event.target.id.includes(pattern))) {
                     //get Security Material and Key Store entries
                     //$format is ignored in both requests, so we need to set accept in order to get a json response
                     var urlForKeyStoreEntries = `/${pluginHelper.urlExtension}odata/api/v1/KeystoreEntries`;
@@ -154,6 +151,9 @@ var plugin = {
 
                     //add an event listener to the credential name input field for typing
                     event.target.addEventListener('input', handleTyping);
+
+                    //display on click
+                    handleTyping(event);
                 }
             });
 
