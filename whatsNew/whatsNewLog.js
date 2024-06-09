032f6a78fbb304a6d1dd62c3e357f85235a42045
Dominic Beckbauer <github@beckbauer.de>
2024-06-09 15:41:49 +0200

    revert whatsnew for 3.13.1

--- whatsNew/whatsNewLog.js
+++ whatsNew/whatsNewLog.js
@@ -7,6 +7,38 @@
 const whats_new_log = [
     {
         "header": "Plugin",
+        "description": "Credential Helper Plugin to add a search help for existing Security Material aliases and Key Store entries. Special thanks to Gregor Schütz from <a target='_blank' href='https://www.agilita.ch/'>AGILITA AG</a>"
+    },
+    {
+        "header": "Plugin",
+        "description": "Unlock Plugin to unlock integration flows directly from design screen. Special thanks to Gregor Schütz from <a target='_blank' href='https://www.agilita.ch/'>AGILITA AG</a>"
+    },
+    {
+        "header": "Plugin Improvement",
+        "description": "Timeline Plugin now with sorting options. Again special thanks to Gregor Schütz from <a target='_blank' href='https://www.agilita.ch/'>AGILITA AG</a>"
+    },
+    {
+        "header": "Feature Support",
+        "description": "The CPi helper extension now includes compatibility with the Dark CPI extension. For <b> Dark Theme Users </b> Special thanks to <a target='_blank' href='https://github.com/incpi'>Omkar Patel</a>",
+    },
+    {
+        "header": "General Improvement",
+        "description": "Filter to remove extra Empty Steps from Trace & Tool is now more compact. Special thanks to <a target='_blank' href='https://github.com/incpi'>Omkar Patel</a>",
+    },
+    {
+        "header": "Search Feature",
+        "description": "New global Button for all Logging & Security Materials search with All category supported. Special thanks to <a target='_blank' href='https://github.com/incpi'>Omkar Patel</a>",
+    },
+    {
+        "header": "Debug Feature",
+        "description": "Debug Logging button UI as well as by parameters.",
+    },
+    {
+        "header": "Extension Feature",
+        "description": "Can select default log level at extension page. Special thanks to <a target='_blank' href='https://github.com/incpi'>Omkar Patel</a>",
+    },
+    {
+        "header": "Plugin",
         "description": "Apache Camel Update Message Remover - get rid of the annoying update message"
     },
     {
