34ca62ebf199cf3341d0018c71186a22d05149db
Dominic Beckbauer <github@beckbauer.de>
2024-12-23 21:04:27 +0100

    fix: readme

--- whatsNew/whatsNewLog.js
+++ whatsNew/whatsNewLog.js
@@ -1,5 +1,5 @@
 // template = - [header :your header as HTML] [description": "description goes here as HTML] ,... //multiple
 const whats_new_log = `   
+- [Improvement] Improved performance of the extension.
 - [Fix] Solved problem with activating trace (on non-Edge Cell and Edge Cell tenants).
-- [Fix] Could not beautify logs in some cases
 `;
