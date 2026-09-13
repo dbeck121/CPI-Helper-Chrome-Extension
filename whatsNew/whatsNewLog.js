a68bb18f295d44bb1c05a9bc232c7fddb44347a2
Dominic Beckbauer <github@beckbauer.de>
2026-09-13 16:34:55 +0200

    docs: add MPL Table Sorter and Create named Groovy script to 3.27 release notes

--- whatsNew/whatsNewLog.js
+++ whatsNew/whatsNewLog.js
@@ -1,5 +1,7 @@
 // template = - [header :your header as HTML] [description": "description goes here as HTML] ,... //multiple
 const whats_new_log = `
 - [Plugin] New Version History plugin: view the version history of an iFlow and revert to an earlier version directly in the editor. Special thanks to Gregor Schütz.
+- [Plugin] New MPL Table Sorter plugin: sort the Monitor Message Processing table by one or more columns. Special thanks to Prem Sai Daggolu.
+- [Plugin] New Create named Groovy script plugin: create a Groovy resource with a name of your choice in the current iFlow. Special thanks to Björn Konzmann.
 - [Improvement] Updated plugin metadata for Timeline, Unlock and Credential Helper. Thanks to Gregor Schütz.
 `;
