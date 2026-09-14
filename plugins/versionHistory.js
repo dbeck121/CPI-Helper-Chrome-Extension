3de5b58357f2abed57ac9d4fb98bb857eba56ddc
Gregor Schütz <gregor.b.schuetz@gmail.com>
2026-09-14 15:24:21 +0200

    added back HTML escape

--- plugins/versionHistory.js
+++ plugins/versionHistory.js
@@ -1,6 +1,5 @@
 // Escape server-provided values before they are interpolated into HTML.
-const escapeHtml = (value) =>
-  String(value ?? "").replace(/[&<>"']/g, (character) => ({ "&": "&amp;", "<": "&lt;", ">": "&gt;", '"': "&quot;", "'": "&#39;" }[character]));
+const escapeHtml = (value) => String(value ?? "").replace(/[&<>"']/g, (character) => ({ "&": "&amp;", "<": "&lt;", ">": "&gt;", '"': "&quot;", "'": "&#39;" })[character]);
 
 // Confirm the target version and allow its editable fields to be reviewed.
 function showRevertConfirmation(tenant, workspace, artifact, version, currentVersion) {
@@ -142,12 +141,12 @@ var plugin = {
             const createdDate = new Date(Number(version.createdDate)).toLocaleString();
             const isCurrentVersion = version === currentVersion;
             tableHtml += `<tr ${isCurrentVersion ? (version.state == "workingcopy" ? 'class="red"' : 'class="yellow"') : ""}> 
-                            <td data-label="Comment">${version.comment ?? ""}</td> 
-                            <td data-label="Semantic Version">${version.semanticVersion ?? ""}</td> 
-                            <td data-label="Technical Version">${version.technicalVersion ?? ""}</td> 
-                            <td data-label="Created Date">${createdDate}</td> 
-                            <td data-label="Created By">${version.createdBy ?? ""}</td> 
-                            <td data-label="State">${version.state ?? ""}</td> 
+                            <td data-label="Comment">${escapeHtml(version.comment)}</td> 
+                            <td data-label="Semantic Version">${escapeHtml(version.semanticVersion)}</td> 
+                            <td data-label="Technical Version">${escapeHtml(version.technicalVersion)}</td> 
+                            <td data-label="Created Date">${escapeHtml(createdDate)}</td> 
+                            <td data-label="Created By">${escapeHtml(version.createdBy)}</td> 
+                            <td data-label="State">${escapeHtml(version.state)}</td> 
                             <td data-label="Revert">
                               ${isCurrentVersion ? "Current Version" : `<button class="ui button" data-version-index="${index}">Revert</button>`}
                             </td> 
