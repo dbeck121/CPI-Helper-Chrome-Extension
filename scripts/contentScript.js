8db62d14475d02ea7433b7c5358e3e359f166fc2
Dominic Beckbauer <github@beckbauer.de>
2026-09-26 12:30:47 +0200

    fix(popup): clicking the title no longer collapses the message popup

--- scripts/contentScript.js
+++ scripts/contentScript.js
@@ -1214,7 +1214,7 @@ var sidebar = {
     var elem = document.createElement("div");
     elem.innerHTML = `
     <div id="cpiHelper_contentheader" content="${hostData.count}" >
-      <span id='sidebar_modal_minimize' class='cpiHelper_popupTitle' title='Collapse or expand'><span class='cpiHelper_floatingToolbar_dots'></span>CPI Helper</span>
+      <span id='sidebar_modal_minimize' class='cpiHelper_popupTitle'><span class='cpiHelper_floatingToolbar_dots'></span>CPI Helper</span>
       <button type='button' id='sidebar_modal_close' class='cpiHelper_popupHeaderButton' title='Close' aria-label='Close'>${floatingToolbarIcon("close")}</button>
     </div>
     <div id="outerFrame" >
@@ -1261,24 +1261,6 @@ var sidebar = {
         });
       }
     });
-    //add minimize button on CPI helper title & color match with tenant color
-    var span = document.getElementById("sidebar_modal_minimize");
-    var content_header = document.getElementById("cpiHelper_contentheader");
-    var outerFrame_element = document.getElementById("outerFrame");
-    var borderofouterFrame = getComputedStyle(outerFrame_element).borderRadius.split(" ");
-    span.onclick = () => {
-      if (outerFrame_element.offsetHeight > 0) {
-        content_header.style["min-width"] = getComputedStyle(outerFrame_element).width;
-        outerFrame_element.style.display = "none";
-        content_header.style["border-bottom-left-radius"] = borderofouterFrame[2];
-        content_header.style["border-bottom-right-radius"] = borderofouterFrame[3];
-      } else {
-        outerFrame_element.style.display = "block";
-        content_header.style["border-bottom-left-radius"] = borderofouterFrame[0];
-        content_header.style["border-bottom-right-radius"] = borderofouterFrame[1];
-      }
-    };
-
     //add close button
     var span = document.getElementById("sidebar_modal_close");
     span.onclick = (element) => {
